fe3c386355db9fe44183f5fb3181de67a1e46cd9
Daniel <daniel.martling@hotmail.com>
2024-07-11 17:02:03 +0200

    fix med ikoner

--- icons.js
+++ icons.js
@@ -3,59 +3,59 @@ var icons = {
     firehouseIcon: new L.Icon({
         iconUrl: 'icons/firehouse.svg',
         iconSize:     [24, 24],
-        iconAnchor:   [12, 20], 
+        iconAnchor:   [12, 60], 
         popupAnchor:  [0,-20],
         shadowUrl: 'icons/pinWhite.png',
         shadowSize:   [80, 80],
-        shadowAnchor: [40,35]
+        shadowAnchor: [40,75]
     }),
     windshelterIcon: new L.Icon({
         iconUrl: 'icons/windshelter.svg',
         iconSize:     [30,30],
-        iconAnchor:   [15,15], 
+        iconAnchor:   [15,55], 
         popupAnchor:  [0,-15],
         shadowUrl: 'icons/pinWhite.png',
         shadowSize:   [80, 80],
-        shadowAnchor: [40,30]
+        shadowAnchor: [40,70]
     }),
     campfireringIcon: new L.Icon({
         iconUrl: 'icons/campfirering.svg',
         iconSize:     [40,40],
-        iconAnchor:   [20, 20], 
+        iconAnchor:   [20, 60], 
         popupAnchor:  [0,-20],
         shadowUrl: 'icons/pinWhite.png',
         shadowSize:   [80, 80],
-        shadowAnchor: [40, 30]
+        shadowAnchor: [40, 70]
     }),
     windshelterfireIcon: new L.Icon({
         iconUrl: 'icons/windshelterfire.svg',
         iconSize:     [30,30],
-        iconAnchor:   [15,15], 
+        iconAnchor:   [15,55], 
         popupAnchor:  [0,-15],
         shadowUrl: 'icons/pinWhite.png',
         shadowSize:   [80, 80],
-        shadowAnchor: [40,30]
+        shadowAnchor: [40,70]
     }),
     campfirerockIcon: new L.Icon({
         iconUrl: 'icons/fire1.svg',
         iconSize: [40, 40],
-        iconAnchor: [20, 20],
+        iconAnchor: [20, 60],
         popupAnchor: [0, -20],
         shadowUrl: 'icons/pinWhite.png',
         shadowSize:   [80, 80],
-        shadowAnchor: [40, 30]
+        shadowAnchor: [40, 70]
     }),
     campfireotherIcon: new L.Icon({
         iconUrl: 'icons/otherPin.svg',
         iconSize: [80, 80],
         iconAnchor: [20, 20],
-        popupAnchor: [0, -20]
+        popupAnchor: [0, -40]
     }),
     startIcon: new L.Icon({
         iconUrl: 'icons/flagStart.svg',
         iconSize: [60, 60],
         iconAnchor: [15, 50],
-        popupAnchor: [0, -30]
+        popupAnchor: [-30, -30]
     }),
     finishIcon: new L.Icon({
         iconUrl: 'icons/flagFinish.svg',
@@ -73,6 +73,6 @@ var icons = {
         iconUrl: 'icons/flagTemp.svg',
         iconSize: [60, 60],
         iconAnchor: [0, 30],
-        popupAnchor: [0, -20]
+        popupAnchor: [0, -30]
     })
 };
