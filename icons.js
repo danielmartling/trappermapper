ca863be7095c7058ff17b1fa3f4dcd11cc6d11da
Gunnar Arakangas <gunnar@arakangas.se>
2024-09-08 18:33:59 +0200

    Changes for PHP and DB usage
    Changed index.html to index.php and renamed icons dir to mapIcons since Apache autoindexing does not play nice with the name "icons"

--- icons.js
+++ icons.js
@@ -1,73 +1,73 @@
 // icons.js
 var icons = {
     firehouseIcon: new L.Icon({
-        iconUrl: 'icons/firehouse.svg',
+        iconUrl: 'mapIcons/firehouse.svg',
         iconSize:     [24, 24],
         iconAnchor:   [12, 60], 
         popupAnchor:  [0,-20],
-        shadowUrl: 'icons/pinWhite.png',
+        shadowUrl: 'mapIcons/pinWhite.png',
         shadowSize:   [80, 80],
         shadowAnchor: [40,75]
     }),
     windshelterIcon: new L.Icon({
-        iconUrl: 'icons/windshelter.svg',
+        iconUrl: 'mapIcons/windshelter.svg',
         iconSize:     [30,30],
         iconAnchor:   [15,55], 
         popupAnchor:  [0,-15],
-        shadowUrl: 'icons/pinWhite.png',
+        shadowUrl: 'mapIcons/pinWhite.png',
         shadowSize:   [80, 80],
         shadowAnchor: [40,70]
     }),
     campfireringIcon: new L.Icon({
-        iconUrl: 'icons/campfirering.svg',
+        iconUrl: 'mapIcons/campfirering.svg',
         iconSize:     [40,40],
         iconAnchor:   [20, 60], 
         popupAnchor:  [0,-20],
-        shadowUrl: 'icons/pinWhite.png',
+        shadowUrl: 'mapIcons/pinWhite.png',
         shadowSize:   [80, 80],
         shadowAnchor: [40, 70]
     }),
     windshelterfireIcon: new L.Icon({
-        iconUrl: 'icons/windshelterfire.svg',
+        iconUrl: 'mapIcons/windshelterfire.svg',
         iconSize:     [30,30],
         iconAnchor:   [15,55], 
         popupAnchor:  [0,-15],
-        shadowUrl: 'icons/pinWhite.png',
+        shadowUrl: 'mapIcons/pinWhite.png',
         shadowSize:   [80, 80],
         shadowAnchor: [40,70]
     }),
     campfirerockIcon: new L.Icon({
-        iconUrl: 'icons/newcampfire.png',
+        iconUrl: 'mapIcons/newcampfire.png',
         iconSize: [60,60],
         iconAnchor: [30,60],
         popupAnchor: [0,-60]
     }),
     campfireotherIcon: new L.Icon({
-        iconUrl: 'icons/otherPin.svg',
+        iconUrl: 'mapIcons/otherPin.svg',
         iconSize: [80, 80],
         iconAnchor: [40, 80],
         popupAnchor: [0, -40]
     }),
     startIcon: new L.Icon({
-        iconUrl: 'icons/flagStart.svg',
+        iconUrl: 'mapIcons/flagStart.svg',
         iconSize: [60, 60],
         iconAnchor: [15, 50],
         popupAnchor: [-30, -30]
     }),
     finishIcon: new L.Icon({
-        iconUrl: 'icons/flagFinish.svg',
+        iconUrl: 'mapIcons/flagFinish.svg',
         iconSize: [60, 60],
         iconAnchor: [15, 50],
         popupAnchor: [0, -30]
     }),
     activityIcon: new L.Icon({
-        iconUrl: 'icons/flagHollyrosa.svg',
+        iconUrl: 'mapIcons/flagHollyrosa.svg',
         iconSize: [60, 60],
         iconAnchor: [15, 50],
         popupAnchor: [0, -30]
     }),
     tempIcon: new L.icon({
-        iconUrl: 'icons/flagTemp.svg',
+        iconUrl: 'mapIcons/flagTemp.svg',
         iconSize: [60, 60],
         iconAnchor: [0, 30],
         popupAnchor: [0, -30]
