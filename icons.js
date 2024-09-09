fcbdf94b43477c7ba29d9bb9b236c25ba9732d55
Joakim Hertzberg <joakim.d.hertzberg@gmail.com>
2024-09-09 21:35:51 +0200

    Lade till dass och soprum

--- icons.js
+++ icons.js
@@ -1,75 +1,88 @@
 // icons.js
 var icons = {
-    firehouseIcon: new L.Icon({
-        iconUrl: 'icons/firehouse.svg',
-        iconSize:     [24, 24],
-        iconAnchor:   [12, 60], 
-        popupAnchor:  [0,-20],
-        shadowUrl: 'icons/pinWhite.png',
-        shadowSize:   [80, 80],
-        shadowAnchor: [40,75]
-    }),
-    windshelterIcon: new L.Icon({
-        iconUrl: 'icons/windshelter.svg',
-        iconSize:     [30,30],
-        iconAnchor:   [15,55], 
-        popupAnchor:  [0,-15],
-        shadowUrl: 'icons/pinWhite.png',
-        shadowSize:   [80, 80],
-        shadowAnchor: [40,70]
-    }),
-    campfireringIcon: new L.Icon({
-        iconUrl: 'icons/campfirering.svg',
-        iconSize:     [40,40],
-        iconAnchor:   [20, 60], 
-        popupAnchor:  [0,-20],
-        shadowUrl: 'icons/pinWhite.png',
-        shadowSize:   [80, 80],
-        shadowAnchor: [40, 70]
-    }),
-    windshelterfireIcon: new L.Icon({
-        iconUrl: 'icons/windshelterfire.svg',
-        iconSize:     [30,30],
-        iconAnchor:   [15,55], 
-        popupAnchor:  [0,-15],
-        shadowUrl: 'icons/pinWhite.png',
-        shadowSize:   [80, 80],
-        shadowAnchor: [40,70]
-    }),
-    campfirerockIcon: new L.Icon({
-        iconUrl: 'icons/newcampfire.png',
-        iconSize: [60,60],
-        iconAnchor: [30,60],
-        popupAnchor: [0,-60]
-    }),
-    campfireotherIcon: new L.Icon({
-        iconUrl: 'icons/otherPin.svg',
-        iconSize: [80, 80],
-        iconAnchor: [40, 80],
-        popupAnchor: [0, -40]
-    }),
-    startIcon: new L.Icon({
-        iconUrl: 'icons/flagStart.svg',
-        iconSize: [60, 60],
-        iconAnchor: [15, 50],
-        popupAnchor: [-30, -30]
-    }),
-    finishIcon: new L.Icon({
-        iconUrl: 'icons/flagFinish.svg',
-        iconSize: [60, 60],
-        iconAnchor: [15, 50],
-        popupAnchor: [0, -30]
-    }),
-    activityIcon: new L.Icon({
-        iconUrl: 'icons/flagHollyrosa.svg',
-        iconSize: [60, 60],
-        iconAnchor: [15, 50],
-        popupAnchor: [0, -30]
-    }),
-    tempIcon: new L.icon({
-        iconUrl: 'icons/flagTemp.svg',
-        iconSize: [60, 60],
-        iconAnchor: [0, 30],
-        popupAnchor: [0, -30]
-    })
+
+  firehouseIcon: new L.Icon({
+    iconUrl: "icons/firehouse.svg",
+    iconSize: [24, 24],
+    iconAnchor: [12, 60],
+    popupAnchor: [0, -20],
+    shadowUrl: "icons/pinWhite.png",
+    shadowSize: [80, 80],
+    shadowAnchor: [40, 75],
+  }),
+  windshelterIcon: new L.Icon({
+    iconUrl: "icons/windshelter.svg",
+    iconSize: [30, 30],
+    iconAnchor: [15, 55],
+    popupAnchor: [0, -15],
+    shadowUrl: "icons/pinWhite.png",
+    shadowSize: [80, 80],
+    shadowAnchor: [40, 70],
+  }),
+  campfireringIcon: new L.Icon({
+    iconUrl: "icons/campfirering.svg",
+    iconSize: [40, 40],
+    iconAnchor: [20, 60],
+    popupAnchor: [0, -20],
+    shadowUrl: "icons/pinWhite.png",
+    shadowSize: [80, 80],
+    shadowAnchor: [40, 70],
+  }),
+  windshelterfireIcon: new L.Icon({
+    iconUrl: "icons/windshelterfire.svg",
+    iconSize: [30, 30],
+    iconAnchor: [15, 55],
+    popupAnchor: [0, -15],
+    shadowUrl: "icons/pinWhite.png",
+    shadowSize: [80, 80],
+    shadowAnchor: [40, 70],
+  }),
+  campfirerockIcon: new L.Icon({
+    iconUrl: "icons/newcampfire.png",
+    iconSize: [60, 60],
+    iconAnchor: [30, 60],
+    popupAnchor: [0, -60],
+  }),
+  campfireotherIcon: new L.Icon({
+    iconUrl: "icons/otherPin.svg",
+    iconSize: [80, 80],
+    iconAnchor: [40, 80],
+    popupAnchor: [0, -40],
+  }),
+  startIcon: new L.Icon({
+    iconUrl: "icons/flagStart.svg",
+    iconSize: [60, 60],
+    iconAnchor: [15, 50],
+    popupAnchor: [-30, -30],
+  }),
+  finishIcon: new L.Icon({
+    iconUrl: "icons/flagFinish.svg",
+    iconSize: [60, 60],
+    iconAnchor: [15, 50],
+    popupAnchor: [0, -30],
+  }),
+  activityIcon: new L.Icon({
+    iconUrl: "icons/flagHollyrosa.svg",
+    iconSize: [60, 60],
+    iconAnchor: [15, 50],
+    popupAnchor: [0, -30],
+  }),
+  tempIcon: new L.icon({
+    iconUrl: "icons/flagTemp.svg",
+    iconSize: [60, 60],
+    iconAnchor: [0, 30],
+    popupAnchor: [0, -30],
+  }),
+  trashroomIcon: new L.icon({
+    iconUrl: "icons/trashroom.png",
+    iconSize: [60, 60],
+    iconAnchor: [30, 60],
+    popupAnchor: [0, -60],
+  }),
+  toiletIcon: new L.icon({
+    iconUrl: "icons/toilet.png",
+    iconSize: [60, 60],
+    iconAnchor: [30, 60],
+    popupAnchor: [0, -60],
+  }),
 };
