e5f138bbe3e5ec1fa6e50c348f08d36ad036483e
Daniel Martling <140479913+danielmartling@users.noreply.github.com>
2025-02-16 21:15:21 +0100

    Update maplayers.js

--- maplayers.js
+++ maplayers.js
@@ -4,7 +4,7 @@ var basemaps = {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors.</a>.'
     }),
     Satellit: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+        attribution: 'Esri, Maxar, Earthstar Geographics, and the GIS User Community'
     })
 };
 
